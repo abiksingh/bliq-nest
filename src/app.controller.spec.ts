8942f2cdd8f1f5b0dea58140e49a401c6f38ed21
Abhi <a.singh@vehiculum.de>
2024-07-30 16:00:17 +0200

    feat: fixed tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,13 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import {AppController} from "./app.controller";
+import { AppController } from './app.controller';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
-
 
 describe('RideController', () => {
   let rideController: AppController;
   let appService: AppService;
-
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,19 +14,22 @@ describe('RideController', () => {
     }).compile();
 
     rideController = module.get<AppController>(AppController);
+    appService = module.get<AppService>(AppService);
   });
 
   it('should return normalized ride data', () => {
     const result = [
       { provider: 'Uber', price: 15, duration: '15 mins', carType: 'Sedan' },
-      { provider: 'Bolt', price: 15, duration: '20 mins', carType: 'SUV' }
+      { provider: 'Bolt', price: 15, duration: '20 mins', carType: 'SUV' },
     ];
+
+    jest.spyOn(appService, 'getRideData').mockImplementation(() => result);
 
     expect(rideController.getRideData()).toEqual(result);
   });
 
   it('should throw an error for malformed data', () => {
-    jest.spyOn(rideController, 'getRideData').mockImplementation(() => {
+    jest.spyOn(appService, 'getRideData').mockImplementation(() => {
       throw new HttpException('Invalid data format', HttpStatus.BAD_REQUEST);
     });
 
@@ -41,7 +42,7 @@ describe('RideController', () => {
   });
 
   it('should handle unexpected errors gracefully', () => {
-    jest.spyOn(rideController, 'getRideData').mockImplementation(() => {
+    jest.spyOn(appService, 'getRideData').mockImplementation(() => {
       throw new Error('Unexpected Error');
     });
 
